1c33b259aa69caecc628e173a9380846a1557a87
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-19 22:00:30 +0900

    fix base model, register error

--- app/routes/user/register.py
+++ app/routes/user/register.py
@@ -13,29 +13,29 @@ from tools import *
 
 class Register_example(BaseModel):
     username: str
-    school_id: str
     password: str
+    handle_name: str
     re_pw: str
 
 @router.post("/api/register", tags=["register"])
 async def register(data: Register_example): 
     if data.password != data.re_pw:
         raise HTTPException(status_code=400, detail="비밀번호가 일치하지 않습니다.")
-
+    
     hashed_pw = hashing_pw(data.password)
 
-    db_user = User( 
-        username=data.username,
-        password=hashed_pw,
-        school_id=data.school_id,
-    )
-
     async with AsyncSessionLocal() as session:  
-
         user = await session.execute(select(User).filter(User.username == data.username))
+        user_info = user.scalars().first()
 
-        if user: 
+        if user_info: 
             raise HTTPException(status_code=400, detail="이미 가입하셨습니다.")
+        
+        db_user = User( 
+            username=data.username,
+            password=hashed_pw,
+            handle_name=data.handle_name
+        )
 
         session.add(db_user)  
         await session.commit()
