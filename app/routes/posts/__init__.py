6f7141612eae62c7fa1c23a7f87db1a9908bc05f
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-19 23:07:31 +0900

    add delete router

--- app/routes/posts/__init__.py
+++ app/routes/posts/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from .get import router as get_router
 from .create import router as create_router
 from .update import router as update_router
+from .delete import router as delete_router
 
 router = APIRouter()
 
 router.include_router(get_router)
 router.include_router(create_router)
 router.include_router(update_router)
+router.include_router(delete_router)    
---
+++ app/routes/posts/delete.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException, Header, Response, APIRouter, Depends
+from pydantic import BaseModel
+from datetime import datetime, timezone     
+from depends import RequireAuth
+from sqlalchemy.future import select 
+from sqlalchemy.sql.expression import desc
+from database.core import AsyncSessionLocal
+from database.user import User
+from database.posts import Posts
+from typing import Optional
+
+router = APIRouter()
+
+@router.post("/api/delete/{post_id}", tags=["create posts"])
+async def submit_apply(userid=Depends(RequireAuth), post_id: int = 0):
+    
+    if not userid:
+        raise HTTPException(status_code=401, detail="로그인 후 이용 가능합니다.")
+    
+    async with AsyncSessionLocal() as session:
+        post_query = select(Posts).where(Posts.id == post_id)
+        post = await session.execute(post_query)
+        post_to_delete = post.scalars().first()
+        
+        if not post_to_delete:
+            raise HTTPException(status_code=404, detail="해당 게시글이 없습니다.")
+        
+        await session.delete(post_to_delete)
+        await session.commit()
+
+    return {"ok": True}
