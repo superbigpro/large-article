dd8b3c93d39756a42302b7ad69010c10e53e6549
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-19 22:48:57 +0900

    add update router

--- app/routes/posts/__init__.py
+++ app/routes/posts/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .get import router as get_router
 from .create import router as create_router
+from .update import router as update_router
 
 router = APIRouter()
 
 router.include_router(get_router)
 router.include_router(create_router)
+router.include_router(update_router)
