eea87c9ef7a186a8572d8d14f27ba7cfb8775a95
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-24 19:29:38 +0900

    fix router init

--- AuthService/app/routes/__init__.py
+++ AuthService/app/routes/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from .posts import router as posts_router
 from .user import router as user_router
 
 
 def include_router(app: FastAPI):
-    app.include_router(posts_router)
     app.include_router(user_router)
