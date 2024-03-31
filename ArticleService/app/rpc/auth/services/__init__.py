b6edcb18edc9ae8ff2892ae0ceddbf69cf527f0c
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-31 21:26:34 +0900

    add grpc codes

--- ArticleService/app/rpc/auth/services/__init__.py
+++ ArticleService/app/rpc/auth/services/__init__.py
@@ -1,2 +1,13 @@
 from .authorize import authorize
 from .getuser import get_user
+
+from rpc.auth.declaration import auth_pb2_grpc
+from .authorize import AuthorizeInterface
+from .getuser import GetUserInterface
+
+class AuthorizeServicer(auth_pb2_grpc.AuthServiceServicer):
+    async def Authorize(self, request, context):
+        return await AuthorizeInterface(self, request, context)
+
+    async def GetUser(self, request, context):
+        return await GetUserInterface(self, request, context)
---
+++ ArticleService/app/rpc/main.py
@@ -0,0 +1,16 @@
+from grpc import aio
+from rpc.auth.declaration import auth_pb2_grpc
+from rpc.auth.services import AuthorizeServicer
+
+
+class gRPCServer:
+    @staticmethod
+    async def run():
+        server = aio.server()
+
+        auth_pb2_grpc.add_AuthServiceServicer_to_server(AuthorizeServicer(), server)
+
+        server.add_insecure_port("[::]:50051")
+        await server.start()
+        print("[GRPC] Server is running on port", 50051)
+        await server.wait_for_termination()
