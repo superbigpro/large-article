5790dada70f114318245c17aead34116eacf3481
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-04-02 23:01:51 +0900

    fix import errorrrrr

--- ArticleService/app/main.py
+++ ArticleService/app/main.py
@@ -6,21 +6,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 app = FastAPI()
 
-from tools import check_auth  
-
-
 async def create_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-
-@app.post("/api/authcheck", tags=["authcheck test"])
-async def authcheck(token : str = Header(...)):
-    user = check_auth(token)
-    
-    if not user:
-        return {"ok": "False"}
-    
-    return {"received_token": token}
-
-include_router(app)
+# include_router(app)
--- ArticleService/app/routes/__init__.py
+++ ArticleService/app/routes/__init__.py
@@ -1,3 +1,5 @@
+# routes/__init__.py
+
 from fastapi import FastAPI
 from .posts import router as posts_router
 from .comments import router as comments_router
