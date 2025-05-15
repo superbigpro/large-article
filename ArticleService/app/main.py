1e9bf170634ebfc662ce3c1ed3ec857b0b04652f
leeyeonwoo <rexrex1105@gmail.com>
2025-05-15 20:46:52 +0900

    refactor: edit init run code

--- ArticleService/app/main.py
+++ ArticleService/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from database.core import async_engine, Base  
 from routes import include_router 
-from rpc import gRPCServer
 import asyncio
 
 app = FastAPI()
@@ -11,9 +10,9 @@ async def create_tables():
         await conn.run_sync(Base.metadata.create_all)
 
 async def main():
-    await gRPCServer.run()
+    await create_tables()
 
 if __name__ == "__main__":
-    asyncio.run(main(), create_tables())
+    asyncio.run(main())
 
 include_router(app)
