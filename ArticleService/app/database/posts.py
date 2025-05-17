e53caa298c3c7c068b43c8c790d601befd9f2542
leeyeonwoo <rexrex1105@gmail.com>
2025-05-17 20:31:52 +0900

    feat(table): add view, hearts column on Posts table

--- ArticleService/app/database/posts.py
+++ ArticleService/app/database/posts.py
@@ -7,14 +7,14 @@ from database import Base
 
 class Posts(Base):
     __tablename__ = "posts"
-    # 고유 id 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    # 제목 
     title = Column(String, nullable=False)  
     content = Column(String, nullable=False)  
     picture = Column(BLOB, nullable=True)  
     last_modified = Column(DateTime, nullable=True, onupdate=datetime.now(timezone.utc), default=datetime.now(timezone.utc))
     is_modified = Column(Boolean, nullable=False, default=False)
+    views = Column(Integer, nullable=False, default=0)
+    hearts = Column(Integer, nullable=False, default=0)
 
     comments = relationship('Comments', back_populates='post')
     user_id = Column(BigInteger, nullable=False)
