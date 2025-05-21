9a2ca1d0e86498a44b3e375c7ac294dd48b5b292
leeyeonwoo <rexrex1105@gmail.com>
2025-05-21 14:03:32 +0900

    feat(logger): add logger on gRPC Entrypoint

--- ArticleService/app/grpc-main.py
+++ ArticleService/app/grpc-main.py
@@ -1,9 +1,18 @@
 from rpc import gRPCServer
 import asyncio
+import logging
 
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("grpc-main")
 
 async def main():
-    await gRPCServer.run()
+    try:
+        logger.info("Starting Article gRPC server...")
+        await gRPCServer.run()
+    except Exception as e:
+        logger.error(f"Error starting gRPC server: {str(e)}")
+        raise
 
 
 if __name__ == "__main__":
